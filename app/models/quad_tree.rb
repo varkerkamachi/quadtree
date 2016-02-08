318b4730068eb4349afa22bb0f5204c04c62c1a5
varkerkamachi <andrewtcranston@gmail.com>
2016-02-07 23:56:09 -0800

    modifications to node and quad_tree models - fixing array flatten issue

--- app/models/quad_tree.rb
+++ app/models/quad_tree.rb
@@ -2,7 +2,8 @@ class QuadTree
 
   #has_many :nodes
 
-  attr_accessor :nodes, :node_data, :y1, :x1, :y2, :x2, :node_count
+  #attr_accessor :nodes, :node_data, :y1, :x1, :y2, :x2, :capacity, :node_count, :node_data
+  attr_accessor :nodes, :node_data, :node_count
   # need to build initial tree bounding box ... 
   def initialize coords = {}
     return nil if coords == {}
@@ -11,7 +12,7 @@ class QuadTree
     @y1 = coords[:y1] rescue nil
     @y2 = coords[:y2] rescue nil
     @node_count = 0
-    @nodes = []
+    @nodes = [] if @nodes.blank?
     @node_data = nil
   end
 
@@ -22,15 +23,11 @@ class QuadTree
 
   # creates 4 nodes within initial bounding box
   def create_nodes
-    if @nodes.blank?
-      4.times do |x|
-        coords = calculate_coords(x)
-        @nodes << Node.new(coords)
-        @node_count += 1
-      end
-    #else
+    4.times do |x|
+      coords = calculate_coords(x)
+      @nodes << Node.new(coords)
+      @node_count += 1
     end
-    @nodes
   end
 
   # calculate the xy coords based on integer passed in from iteration loop. should be 0-3
@@ -64,7 +61,6 @@ class QuadTree
       coords[:x2] = @x2
       coords[:y2] = @y2
     end
-    puts "coords: #{coords.inspect}"
 
     coords
   end
@@ -76,22 +72,35 @@ class QuadTree
     if @nodes.blank?
       create_nodes
     end
-    puts "Nodes; #{@nodes.inspect}"
     # iterate through set of nodes checking for correct placement of data
     @nodes.each do |n|
       @selected_node = n if locate_correct_node_for_data(n,x,y)
     end
     return false if @selected_node.blank?
-    puts "sel Nodes; #{@selected_node.inspect}"
 
-    if @selected_node.present? && @selected_node.try(:is_full?)
+    create_and_select_node_for_insertion(@selected_node,x,y)
+
+    @selected_node.node_data = DataPoint.new(x,y)
+    @selected_node.capacity += 1
+
+  end
+
+  def create_and_select_node_for_insertion(node,x,y)
+    # check_node_level(@selected_node)
+    if node.try(:is_full?)
+      @selected_node = nil
+      @subnodes = [] if @subnodes.blank?
       # create sub-nodes here .......
-      @nodes = @selected_node.create_nodes
-    else
-      @selected_node.node_data = DataPoint.new(x,y)
-      @selected_node.capacity += 1
-    end
+      @subnodes.push(node.create_nodes).try(:flatten)
 
+      @subnodes.flatten.each do |n|
+        @selected_node = n if locate_correct_node_for_data(n,x,y)
+      end
+      @nodes.push(@subnodes)
+      @nodes = @nodes.try(:flatten)
+      create_and_select_node_for_insertion(@selected_node,x,y)
+    end
+    @selected_node
   end
 
   #calculates which node the data point belongs in
